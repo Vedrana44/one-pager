0c5e3f7da9de4ecd0ccd3e7a101bf2463c76d6c7
Vedrana44 <vedrana.vekic4@gmail.com>
2017-12-14 18:16:03 +0100

    developement of the sticky navigation, further styling

---
+++ app/js/main.js
@@ -0,0 +1,34 @@
+//STICKY NAVIGATION
+
+window.onscroll = function() {stickyNavigation()};
+
+var navigation = document.getElementById('navigation');
+var sticky = navigation.offsetTop;
+
+function stickyNavigation() {
+    if (window.pageYOffset >= sticky) {
+        navigation.classList.add("sticky")
+    } else {
+        navigation.classList.remove("sticky");
+    }
+}
+
+
+//ANIMATED NAVIGATION
+$(document).ready(function(){
+    $("nav a, .hero a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+          $('html, body').animate({
+          scrollTop: $(hash).offset().top - 82
+        }, 600, function(){
+          window.location.hash = hash - 82;
+        });
+      }
+    });
+});
+
+$(window).on('hashchange', function(e){
+    history.replaceState ("", document.title, e.originalEvent.oldURL);
+});
