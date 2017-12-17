19b2f3cbb356a19340b8a92d215a6ebb6a4cb6c9
Vedrana44 <vedrana.vekic4@gmail.com>
2017-12-17 16:43:42 +0100

    Ajax form post

--- app/js/main.js
+++ app/js/main.js
@@ -15,6 +15,7 @@ function stickyNavigation() {
 
 
 //ANIMATED NAVIGATION
+
 $(document).ready(function(){
     $("nav a, .hero a").on('click', function(event) {
       if (this.hash !== "") {
@@ -31,8 +32,40 @@ $(document).ready(function(){
 
 
 //REMOVE ID FROM URL
+
 $(window).on('hashchange', function(e){
     history.replaceState ("", document.title, e.originalEvent.oldURL);
 });
 
 
+//POSTING FORM DATA
+
+function submit() {
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    if(name && email && message){
+      $.ajax
+      ({
+        type: 'post',
+        url: 'post.php',
+        data: 
+        {
+           name: name,
+           email: email,
+           message: message
+        },
+        success: function (response) 
+        {
+          document.getElementById("feedback").innerHTML = "Thank you, form is submitted successfully.";
+          document.getElementById("form").reset();
+        }
+      });
+    }
+    else {
+        document.getElementById("feedback").innerHTML = "Please fill all data!";
+    }
+
+}
